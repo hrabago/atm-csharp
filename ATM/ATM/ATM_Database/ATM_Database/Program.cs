79c8671005d15fe594778aab434c4b2c249d821d
Madeline Placik <madelineplacik@Madelines-MacBook-Pro.local>
2019-03-04 18:44:18 -0500

    changes to database

--- ATM/ATM/ATM_Database/ATM_Database/Program.cs
+++ ATM/ATM/ATM_Database/ATM_Database/Program.cs
@@ -9,9 +9,12 @@ namespace ATM_Database
     {
 
         //public StringBuilder csv = new StringBuilder();
-        public static string userName ="maddy";
-        public static string passWord = "pass";
-        public static float balance = 0.0f;
+        //public static string userName ="maddy";
+        //public static string passWord = "pass";
+        //public static float balance = 0.0f;
+        char[] splitWith = { ' ', ',', '{', '}', ';' };
+        string line;
+        string bal;
 
         internal static string GetStringSha256Hash(string text)
         {
@@ -26,10 +29,62 @@ namespace ATM_Database
             }
         }
 
-        static void Main(string[] args)
+        public string FetchBalance(string filepath, string username, string password)
         {
+           
+            using (StreamReader sr = new StreamReader(filepath))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] data = line.Split(splitWith);
+                    if (data[0] == username)
+                    {
+                        if (data[1] == GetStringSha256Hash(password))
+                        {
+                            bal = data[2];
+                        }
+                        else
+                        {
+                            Console.WriteLine("wrong password");
+                            bal =  "wrong password";
+                        }
+                    }
+                    Console.WriteLine("username does not exist";
+                    bal =  "username does not exist";
+                }
+            }
+            return bal;
+        }
 
-            //write to new file
+        public void UpdateBalance(string filepath,string username, string password, float balance)
+        {
+          
+
+            using (StreamReader sr = new StreamReader(filepath))
+            {
+                while((line = sr.ReadLine())!= null)
+                {
+                    string[] data = line.Split(splitWith);
+                    if(data[0] == username)
+                    {
+                        if(data[1] == GetStringSha256Hash(password))
+                        {
+                            data[3] = balance.ToString();
+                        }
+                        else
+                        {
+                            Console.WriteLine("wrong password");
+                        }
+                    }
+                    Console.WriteLine("username does not exist");
+                }
+
+            }
+        }
+
+        public void AddNewUser(string database, string username, string password)
+        {
+            //if database doesnt exist
             string path = "/Users/madelineplacik/Projects/database.txt";
             if (!File.Exists(path))
             {
@@ -37,29 +92,62 @@ namespace ATM_Database
                 using (StreamWriter sw = File.CreateText(path))
                 {
                     sw.WriteLine("{Username, Password, Balance};");
-                    sw.WriteLine("{" + userName + ", " + GetStringSha256Hash(passWord) + ", " + balance + "}; ");
+                    sw.WriteLine("{" + username + ", " + GetStringSha256Hash(password) + ", " + 0.00 + "}; ");
                 }
-
             }
-
-            //append to file that already exists
+            //add to existing database
             if (File.Exists(path))
             {
                 //if username doesnt exist, add it 
-                using(StreamReader sr = new StreamReader(path))
+                using (StreamReader sr = new StreamReader(path))
                 {
                     string contents = sr.ReadToEnd();
-                    if (!contents.Contains(userName))
+                    if (!contents.Contains(username))
                     {
                         using (StreamWriter sw = File.AppendText(path))
                         {
-                            sw.WriteLine("{" + userName + ", " + GetStringSha256Hash(passWord) + ", " + balance + "}; ");
+                            sw.WriteLine("{" + username + ", " + GetStringSha256Hash(password) + ", " + 0.00 + "}; ");
                         }
                     }
 
                 }
-
             }
+        }
+
+        static void Main(string[] args)
+        {
+
+            ////write to new file
+            //string path = "/Users/madelineplacik/Projects/database.txt";
+            //if (!File.Exists(path))
+            //{
+            //    //add username pw and balance
+            //    using (StreamWriter sw = File.CreateText(path))
+            //    {
+            //        sw.WriteLine("{Username, Password, Balance};");
+            //        sw.WriteLine("{" + userName + ", " + GetStringSha256Hash(passWord) + ", " + balance + "}; ");
+            //    }
+
+            //}
+
+            ////append to file that already exists
+            //if (File.Exists(path))
+            //{
+            //    //if username doesnt exist, add it 
+            //    using(StreamReader sr = new StreamReader(path))
+            //    {
+            //        string contents = sr.ReadToEnd();
+            //        if (!contents.Contains(userName))
+            //        {
+            //            using (StreamWriter sw = File.AppendText(path))
+            //            {
+            //                sw.WriteLine("{" + userName + ", " + GetStringSha256Hash(passWord) + ", " + balance + "}; ");
+            //            }
+            //        }
+
+            //    }
+
+            //}
      
         }
     }
